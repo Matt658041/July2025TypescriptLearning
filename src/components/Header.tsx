ff6ca77d740795f68175a187779da6dac16918e7
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-07-15 13:50:25 -0500

    getting fussy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
 import Button from './UI/Button.tsx';
+import { useTimersContext } from '../store/timer-context.tsx';
+
 
 export default function Header() {
+ const timersCtx = useTimersContext();
+
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{timersCtx.isRunning ? 'Stop Timers' : 'Start Timers'}Timers</Button>
     </header>
   );
 }
